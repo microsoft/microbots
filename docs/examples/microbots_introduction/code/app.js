31097a81f30c24f918ef63aefc8dae67f3c06d3e
shivashanmugam <sivashanmugam4444@gmail.com>
2026-04-20 16:55:16 +0530

    removing app.js file from the examples folder

--- docs/examples/microbots_introduction/code/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// app.ts — A simple greeting function with a syntax error
-function greet(name) {
-    const message = `Hello, ${name}!`;
-    console.log(message);
-}
-+b;
-greet("Microbots");
-console.log(add(2, 3));
